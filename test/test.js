d7a2714b6b81d60558901af17e9157260a923a62
muhwa91 <muhwa91@gmail.com>
2023-12-30 16:01:15 +0900

    1230 프로젝트 진행 테스트

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
-
+// 찜하기 버튼 시작
 document.querySelectorAll('.like-button').forEach(function(element) {
     element.addEventListener('click', function(e) {
         e.preventDefault();
         this.classList.toggle('liked');
     });
 });
+// 찜하기 버튼 끝
